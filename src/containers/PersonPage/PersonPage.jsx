df5bcccf35dc03e6846245902a938cf3ada0288c
kam1kazy <t2nda@yandex.ru>
2022-04-10 21:36:56 +0300

    pesonCard

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,18 +1,71 @@
-import PropType from 'prop-types';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+
+import PersonPhoto from '@components/PersonPage/PersonPhoto';
+import PersonInfo from '@components/PersonPage/PersonInfo';
+
+import { API_PERSON } from '@constants/api'
+import { withErrorApi } from '@hoc-helpers/withErrorApi'
+import { getApiResource } from '@utils/network'
+import { getPeopleImage } from '@services/getPeopleData'
+
 import styles from './PersonPage.module.css';
 
-const PersonPage = ( {id} ) => {
+const PersonPage = ({ match, setErrorApi }) => {
+    const [personInfo, setPersonInfo] = useState(null);
+    const [personName, setPersonName] = useState(null);
+    const [personPhoto, setPersonPhoto] = useState(null)
+    match = useParams().id;
+ 
+    useEffect(() => {
+        (async () => {
+            const res = await getApiResource(`${API_PERSON}/${match}/`);
+
+            if (res) {
+                setPersonInfo([
+                   { title: 'Height: ', data: res.height },
+                   { title: 'Mass: ', data: res.mass },
+                   { title: 'Skin_color: ', data: res.skin_color },
+                   { title: 'Hair_color: ', data: res.hair_color },
+                   { title: 'Eye_color: ', data: res.eye_color },
+                   { title: 'Birth_year: ', data: res.birth_year },
+                   { title: 'Gender: ', data: res.gender },
+                ])
+                setPersonName(res.name)
+                setPersonPhoto(getPeopleImage(match))
+                // res.film 
+
+                setErrorApi(false)
+            } else {
+                setErrorApi(true)
+            }
+
+        })();
+    }, []);
 
     return (
         <>
-            <h1 className={styles.title}>Person Page -  {useParams().id} </h1>
+            <div className={styles.wrapper}>
+                <span className={styles.person__name}>{personName}</span> 
+
+                <div className={styles.container}>
+
+                    <PersonPhoto 
+                        personPhoto={personPhoto}
+                        personName={personName} 
+                    />
+
+                    {personInfo && <PersonInfo personInfo={personInfo} />}
+                </div>
+            </div>
         </>
     )
 }
 
 PersonPage.prototype = {
-    // 
+    setErrorApi: PropTypes.func,
+    match: PropTypes.object
 }
 
-export default PersonPage;
+export default withErrorApi(PersonPage);
